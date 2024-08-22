2197280240003e39977cd8f6ecefe80d4e56d6c6
Elham <elham.chatrabnoos@gmail.com>
2024-08-22 14:01:46 +0330

    some sorting

--- lib/home/views/pages/home_page.dart
+++ lib/home/views/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sockettest/home/views/widgets/main_list_widget.dart';
 
 import '../../controllers/home_page_controller.dart';
 import '../widgets/custom_text.dart';
@@ -74,47 +75,7 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(height: 20),
-                    Expanded(
-                      child: Obx(() {
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: ListView(
-                                shrinkWrap: true,
-                                children: controller.receivedMessage1.map(
-                                  (element) {
-                                    return Container(
-                                      padding: const EdgeInsets.all(10),
-                                      margin: const EdgeInsets.all(10),
-                                      decoration: BoxDecoration(
-                                        color: Colors.white.withOpacity(0.3),
-                                        borderRadius: BorderRadius.circular(5),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.amber.withOpacity(0.2),
-                                            spreadRadius: 5,
-                                            blurRadius: 12,
-                                            offset: const Offset(4, 4),
-                                          ),
-                                        ],
-                                      ),
-                                      child: Text(
-                                        element,
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ).toList(),
-                              ),
-                            ),
-                          ],
-                        );
-                      }),
-                    ),
+                    Expanded(child: MainListWidget()),
                   ],
                 ),
               ),
---
+++ lib/home/views/widgets/main_list_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sockettest/home/controllers/home_page_controller.dart';
+import 'package:sockettest/home/views/pages/home_page.dart';
+
+class MainListWidget extends GetView<HomePageController> {
+  const MainListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: controller.receivedMessage1.map(
+                    (element) {
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.amber.withOpacity(0.2),
+                          spreadRadius: 5,
+                          blurRadius: 12,
+                          offset: const Offset(4, 4),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      element,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
+        ],
+      );
+    });
+  }
+}
