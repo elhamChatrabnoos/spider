cbf480b38016b2213edb03c129b8f4a9e4ca4aea
Elham <elham.chatrabnoos@gmail.com>
2024-10-24 18:10:04 +0330

    impl accounts page ui and connect its api

--- lib/features/accounts/views/pages/accounts_page.dart
+++ lib/features/accounts/views/pages/accounts_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sockettest/app/network/request_status.dart';
-import 'package:sockettest/app/network/response_status.dart';
 import 'package:sockettest/features/accounts/controllers/accounts_page_controller.dart';
 import 'package:sockettest/features/accounts/views/widgets/account_item.dart';
 import 'package:sockettest/features/accounts/views/widgets/add_edit_account_dialog.dart';
